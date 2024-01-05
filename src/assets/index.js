8f6d00d7e31dcf8425786131e3450abf00083dbe
AlbertXylonWang <[Albertxylonwang@gmail.com]>
2024-01-05 06:13:34 -0600

    updated png imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,9 +4,8 @@ import whiteabstract from './backgrounds/white-abstract.png';
 import worldmap from './backgrounds/world-map.png';
 
 import logo from './logo/logo-black.png';
-import logotext from './logo/logo-text-black.png';
 
-import shaq from './personal/shaq.png';
+
 
 import backend from './icons/backend.png';
 import ux from './icons/ux.png';
@@ -56,8 +55,6 @@ export {
   whiteabstract,
   worldmap,
   logo,
-  logotext,
-  shaq,
   backend,
   ux,
   frontend,
