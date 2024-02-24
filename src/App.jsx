68f41e1083dff0fe023f68df461c19cbba387309
AlbertXylonWang <[Albertxylonwang@gmail.com]>
2024-02-24 00:40:26 -0600

    updated information

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import {
   About,
   Contact,
-  Experience,
   Hero,
   Navbar,
   Tech,
@@ -34,7 +33,7 @@ const App = () => {
           <div
             className="bg-experienceLight bg-cover bg-center 
             bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
-            <Experience />
+            
           </div>
         </div>
         <div className="relative z-0">
